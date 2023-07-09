ac5448e88eed0d260bb9f2d5be7806b153226aac
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2023-07-09 13:36:00 -0300

    feat/modularization

---
+++ vite-project/src/journalApp.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+
+const JournalApp = () => {
+  return (
+    <div>
+      <p>Modularizando las carpetas </p>
+    </div>
+  )
+} 
+
+export default JournalApp
+
